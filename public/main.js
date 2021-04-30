72740392f73b35212306275bf2b5daa5a585ebad
Joe Myers <joecaricature@gmail.com>
2021-04-29 20:35:45 -0400

    Basic HTML and JS for button events

--- public/main.js
+++ public/main.js
@@ -1,7 +1,49 @@
+function playerOneChoosesRock() {
+  playerOneChoice === 'r'
+}
+function playerOneChoosesPaper() {
+  playerOneChoice === 'p'
+}
+function playerOneChoosesScissors() {
+  playerOneChoice === 's'
+}
+
+function playerTwoChoosesRock() {
+  playerTwoChoice === 'r'
+}
+function playerTwoChoosesPaper() {
+  playerTwoChoice === 'p'
+}
+function playerTwoChoosesScissors() {
+  playerTwoChoice === 's'
+}
+
+let playerOneChoice
+let playerTwoChoice
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  document
+    .querySelector('.playerOne .rock')
+    .addEventListener('click', playerOneChoosesRock)
+
+  document
+    .querySelector('.playerOne .paper')
+    .addEventListener('click', playerOneChoosesPaper)
+
+  document
+    .querySelector('.playerOne .scissors')
+    .addEventListener('click', playerOneChoosesScissors)
+
+  document
+    .querySelector('.playerTwo .rock')
+    .addEventListener('click', playerTwoChoosesRock)
+
+  document
+    .querySelector('.playerTwo .paper')
+    .addEventListener('click', playerTwoChoosesPaper)
+
+  document
+    .querySelector('.playerTwo .scissors')
+    .addEventListener('click', playerTwoChoosesScissors)
 }
 
 document.addEventListener('DOMContentLoaded', main)
